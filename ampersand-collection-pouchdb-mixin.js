b074e322bdbef225785a8c4f4e360c1dd5171dde
Sven Lito <me@svenlito.com>
2014-09-02 15:12:20 +0200

    chore(minor): cleanup

--- ampersand-collection-pouchdb-mixin.js
+++ ampersand-collection-pouchdb-mixin.js
@@ -1,19 +1,9 @@
 var pouchSync = require('ampersand-pouchdb-sync');
 var extend = require('extend-object');
 
-
-// Wrap an optional error callback with a fallback error event.
-var wrapError = function(model, options) {
-  var error = options.error;
-  options.error = function(resp) {
-    if (error) error(model, resp, options);
-    model.trigger('error', model, resp, options);
-  };
-};
-
 module.exports = {
 
-  constructor: function () {
+  initialize: function () {
     this.sync = pouchSync.apply(this, [this.pouch]);
   },
 
